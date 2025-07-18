78d4450df734f21fd9dbcbf3add26c6cb0846807
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2025-07-18 15:23:15 +0500

    Deployee Laravel applications on live server

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,6 +44,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->prefix(env('APP_DIR'))
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
