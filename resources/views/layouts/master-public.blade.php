aa84f644d7802cfc151cc8732a1b763097495162
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2025-07-23 17:29:50 +0500

    Sco Tags update

--- resources/views/layouts/master-public.blade.php
+++ resources/views/layouts/master-public.blade.php
@@ -5,12 +5,27 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="keywords" content="AJK, Youth Loan, PMYP, Small Industries" />
-    <meta property="og:title" content="@yield('title') - PMYP">
-    <meta property="og:image" content="{{ asset('images/logo.png') }}">
-    <meta property="og:description" content="Prime Minister Youth Loan Program for AJK Small Industries">
-    <title>@yield('title') - PMYP</title>
-    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
+
+    {{-- Dynamic Meta Tags --}}
+    <title>@yield('title', 'Prime Minister Youth Loan Program AJK') - PMYP</title>
+    <meta name="description" content="@yield('meta_description', 'Prime Minister Youth Loan Program. Empowering youth through accessible business loan under the vision of the Prime Minister.')">
+    <meta name="keywords" content="@yield('meta_keywords', 'AJK, Youth Loan, PMYLP, AK Small Industries')">
+
+    {{-- Open Graph for Social Sharing --}}
+    <meta property="og:title" content="@yield('og_title', 'Prime Minister Youth Loan Program AJK ')">
+    <meta property="og:description" content="@yield('og_description', 'Prime Minister Youth Loan Program. Empowering youth through accessible business loan under the vision of the Prime Minister.')">
+    <meta property="og:image" content="@yield('og_image', asset('/assets/img/public/logo.png'))">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <link rel="canonical" href="{{ url()->current() }}">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="@yield('og_title', 'Prime Minister Youth Loan Program AJK ')">
+    <meta name="twitter:description" content="@yield('og_description', 'Prime Minister Youth Loan Program. Empowering youth through accessible business loan under the vision of the Prime Minister.')">
+    <meta name="twitter:image" content="@yield('og_image', asset('/assets/img/public/logo.png'))">
+
+
+
+    <link rel="icon" href="{{ asset('/assets/img/public/logo.png') }}" type="image/png">
     @include('include.head-public')
     @yield('style')
 </head>
