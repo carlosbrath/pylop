160ad1950a896dd8597f510a58594ace9d78dc7a
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2025-08-22 16:30:13 +0500

    master file update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>@yield('title') - Visit Ajk</title>
+        <title>@yield('title') - PMYLP</title>
         <link rel="icon" href="{{ asset('/assets/img/public/logo.png') }}" type="image/png">
         @include('include.head')
         @yield('style')
