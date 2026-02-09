4be62107e57f70dc3c1ecfabac3c542f64f92926
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2026-02-09 11:56:57 +0500

    banners

--- resources/views/include/footer-public.blade.php
+++ resources/views/include/footer-public.blade.php
@@ -111,13 +111,13 @@
 
 <!-- Image Modal -->
 <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl modal-dialog-centered">
+    <div class="modal-dialog modal-l modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-0">
-                <img src="{{ asset('assets/img/public/Front.webp') }}" alt="Image" class="img-fluid w-100">
+                <img src="{{ asset('assets/img/public/banner-feb.webp') }}" alt="Image" class="img-fluid w-100">
             </div>
         </div>
     </div>
